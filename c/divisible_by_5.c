7a2277c1701a78f47e14381e7c1b0fc4486e3459
mohana <mohana20051028@gmaail.com>
2025-02-18 02:48:39 -0800

    Your commit message

--- c/divisible_by_5.c
+++ c/divisible_by_5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+#include<conio.h>
 void check_divisibility(int num) {
     // Expected output is "TRUE" or "FALSE"
 }
-
+printf("%d",&num);
 int main() {
     int num;
     scanf("%d", &num);
